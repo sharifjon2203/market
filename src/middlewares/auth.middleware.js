f3677b96d79a4f4cd09dffcfdf96cd27d6820b9a
sharifjon2203 <sharifjonmunavarov@gmail.com>
2025-04-16 15:37:10 +0500

    basic auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,23 @@
-const defaultAdmin = { login: "admin", password: "admin" };
+import { User } from "../models/user.model.js";
 
-export const authMiddleware = (req, res, next) => {
+
+
+
+// const defaultAdmin = { login: "admin", password: "admin" };
+
+export const authMiddleware = async (req, res, next) => {
     try {
         const str = (req.headers.authorization || "").split(" ")[1]
         const [login, password] = Buffer.from(str, "base64").toString("utf-8").split(":");
 
-        if (login === defaultAdmin.login && password === defaultAdmin.password) {
+        const user = await User.findOne({ email: login }).exec()
+
+        if (!user) {
+            res.status(401).send("Autentification required")
+            return;
+        }
+
+        if (login === user.email && password === user.password) {
             // res.send("access granted")
             next()
             return;
